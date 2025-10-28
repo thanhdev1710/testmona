4a8a586dc046d16a448420b4ac1f2b53ed45f851
thanhdev1710 <chithanh171004@gmail.com>
2025-10-28 15:17:01 +0700

    update

--- src/components/SectionFooterBg.tsx
+++ src/components/SectionFooterBg.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function SectionFooterBg() {
   return (
-    <div className="relative mt-[clamp(40px,18vw,300px)]">
-      <div className="relative w-full md:h-[712px] h-[360px]">
+    <div className="relative xl:mt-[260px] lg:mt-[180px] mt-[100px]">
+      <div className="relative w-full xl:h-[712px] h-[360px]">
         <Image
           alt="Footer Background"
           src="/images/footer.jpg"
@@ -14,11 +14,13 @@ export default function SectionFooterBg() {
         />
         <div className="absolute top-0 left-0 z-3 w-full h-full bg-linear-to-b from-black/40 to-transparent"></div>
       </div>
-      <div className="absolute -top-[315px] z-4 w-full">
+      <div className="absolute 2xl:-top-[282px] xl:-top-[250px] lg:-top-[216px] md:-top-[125px] -top-[92px] z-4 w-full">
         <Section>
           <div>
-            <h2 className="text-effect">AFFILIATE</h2>
-            <h3 className="font-bold xl:text-[48px] text-[36px] xl:w-[65%] w-full uppercase">
+            <h2 className="text-effect 2xl:text-[250px] xl:text-[200px] lg:text-[150px] md:text-[100px] text-[50px]">
+              AFFILIATE
+            </h2>
+            <h3 className="font-bold xl:text-[48px] text-[36px] lg:w-[65%] w-full uppercase">
               Chính sách hoa hồng hấp dẫn cho người giới thiệu
             </h3>
           </div>
